a3bfb84a25f52f77ab122e3daef9998f408908aa
Suporte <julia.pinhe22@gmail.com>
2021-08-04 13:05:47 -0300

    conclusao aula um de JS na DIO

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert("E ai, Js!!");
+
+var nome = "julia pinheiro";
+var idade = 22;
+var cidade = "São Paulo"
+var frase = "Um dia vou ser sênior!"
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(frase.replace("Um dia", "Eu"));
+console.log(frase.toLowerCase());
