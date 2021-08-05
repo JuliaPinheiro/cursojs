2534f394f9a6ff9dd56b55f84b7b5f6c1bbb49c2
Julia Pinheiro <julia.pinhe22@gmail.com>
2021-08-04 22:33:29 -0300

    aula 02 de array

--- main.js
+++ main.js
@@ -1,12 +1,59 @@
-alert("E ai, Js!!");
 
-var nome = "julia pinheiro";
-var idade = 22;
-var cidade = "São Paulo"
-var frase = "Um dia vou ser sênior!"
+//AULA 1
+//alert("E ai, Js!!");
 
-console.log(nome);
-console.log(idade);
-console.log(cidade);
-console.log(frase.replace("Um dia", "Eu"));
-console.log(frase.toLowerCase());
+//var nome = "julia pinheiro";
+//var idade = 22;
+//var cidade = "São Paulo"
+//var frase = "Um dia vou ser sênior!"
+
+//console.log(nome);
+//console.log(idade);
+//console.log(cidade);
+//console.log(frase.replace("Um dia", "Eu"));
+//console.log(frase.toLowerCase());
+
+// ARRAY AULA 2
+
+var lista = ["banana", "uva", "maça", "melancia"];
+
+
+//push acrescenta
+
+lista.push("laranja");
+lista.push("melão");
+
+// pop remove o ultimo elemento
+
+lista.pop();
+
+
+console.log(lista);
+console.log(lista[0]);
+console.log(lista[2]);
+
+
+//tamanho
+console.log(lista.length);
+
+// imprime ao contrario 
+
+console.log(lista.reverse());
+
+
+// toString
+console.log(lista.toString());
+
+
+// consegue colocar o que quer
+
+console.log(lista.join("-"));
+
+
+
+// dicionario
+
+var fruta ={nome: "banana", cor: "amarela"}
+
+console.log(fruta);
+console.log(fruta.cor);
